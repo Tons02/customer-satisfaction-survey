07ad8128dec7ba5b0104c224450ee0b502af4873
Antonio Montilla <system.montilla@gmail.com>
2024-04-16 08:08:33 +0800

    add smsforgetpasswordRequest for reset password for contact details validation also configure the kernel for cleanup of expired tokens

--- app/Http/Controllers/Api/SmsController.php
+++ app/Http/Controllers/Api/SmsController.php
@@ -9,6 +9,7 @@ use App\Functions\GlobalFunction;
 use App\Http\Requests\SmsRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Http;
+use App\Http\Requests\SmsForgetPasswordRequest;
 
 class SmsController extends Controller
 {
@@ -32,9 +33,9 @@ class SmsController extends Controller
         return GlobalFunction::response_function(Message::SMS_OTP_SAVE);
     }
 
-    public function sendverificationcoderesetpassword(SmsRequest $request){
+    public function sendverificationcoderesetpassword(SmsForgetPasswordRequest $request){
         $otp = new Otp();
-        $otpValue = $otp->generate($request->input('mobile_number'), 'numeric', 6, 10);
+        $otpValue = $otp->generate($request->input('mobile_number'), 'numeric', 6, 1);
         
         $otpsms = $otpValue->token;
 
